5ceee16c31c82d0a445ef2d268e907de607b2b21
Jacob Hunsaker <jacob.hunsaker96@gmail.com>
2023-12-05 21:49:34 -0600

    58

---
+++ app/code/Easy/58_Length of Last Word.py
@@ -0,0 +1,43 @@
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+# Example 1:
+
+# Input: s = "Hello World"
+# Output: 5
+# Explanation: The last word is "World" with length 5.
+# Example 2:
+
+# Input: s = "   fly me   to   the moon  "
+# Output: 4
+# Explanation: The last word is "moon" with length 4.
+# Example 3:
+
+# Input: s = "luffy is still joyboy"
+# Output: 6
+# Explanation: The last word is "joyboy" with length 6.
+ 
+# Constraints:
+
+# 1 <= s.length <= 104
+# s consists of only English letters and spaces ' '.
+# There will be at least one word in s.
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Split the string by space
+        # Get the last word
+        # Return the length of the last word
+        return len(s.split()[-1])
+        
+        
+        
+
+print(Solution().lengthOfLastWord("Hello World")); # 5
+print(Solution().lengthOfLastWord("   fly me   to   the moon  ")); # 4
+print(Solution().lengthOfLastWord("luffy is still joyboy")); # 6
