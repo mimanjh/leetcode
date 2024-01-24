88bc9a5e60508638805692af1cc062310ed0fc79
Jacob Hunsaker <jacob.hunsaker96@gmail.com>
2024-01-23 19:48:10 -0600

    66

---
+++ public/code/Easy/66_Plus One.py
@@ -0,0 +1,50 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+# Example 1:
+
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be [1,2,4].
+# Example 2:
+
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+# Incrementing by one gives 4321 + 1 = 4322.
+# Thus, the result should be [4,3,2,2].
+# Example 3:
+
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
+
+
+# Constraints:
+
+
+# 1 <= digits.length <= 100
+# 0 <= digits[i] <= 9
+# digits does not contain any leading 0's.
+
+
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        # join each value in the list and convert to int and add 1
+        int_val = int("".join(map(str, digits))) + 1
+        # splilt each value in the int and store in a list and return it
+        return [int(x) for x in str(int_val)]
+
+
+print(Solution().plusOne([1, 2, 3]))  # [1,2,4]
+print(Solution().plusOne([4, 3, 2, 1]))  # [4,3,2,2]
+print(Solution().plusOne([9]))  # [1,0]
